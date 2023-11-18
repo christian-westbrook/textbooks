cbe93d8c7bb86e7bb9076f01002e5efdc867fcfe
christian-westbrook <christianwestbrook@live.com>
2023-11-18 15:16:26 -0600

    Added hello wold example from chapter 1 of Bare Metal C

---
+++ c/bare-metal-c/chapter01/hello.c
@@ -0,0 +1,16 @@
+/*
+ * Hello World -- not the most complicated program in
+ *      the universe but useful as a starting point.
+ *
+ * Usage:
+ *      1. Run the program.
+ *      2. See the world.
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello World!\n");
+    return (0);
+}
